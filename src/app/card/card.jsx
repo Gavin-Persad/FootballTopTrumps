31bf39f0e048523fe62b0c299098534e14425fd7
Gavin-Persad <gavinapersad@gmail.com>
2024-05-17 17:40:24 +0100

    created EAFC API call

--- src/app/card/card.jsx
+++ src/app/card/card.jsx
@@ -1,11 +1,28 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
 import styles from './card.module.css';
-import react from 'react';
+
+async function getCard() {
+  const response = await fetch('https://drop-api.ea.com/rating/fc-24');
+  const card = await response.json();
+  return card;
+}
 
 export default function Card() {
-    return (
-        <div className={styles.card}>
-            <div className={styles.title}>Football</div>
-            <div className={styles.description}>A football</div>
-        </div>
-    );
+  const [cardData, setCardData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getCard();
+      setCardData(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className={styles.card}>
+      {cardData ? <h1>{cardData}</h1> : <p>Loading...</p>}
+    </div>
+  );
 }
