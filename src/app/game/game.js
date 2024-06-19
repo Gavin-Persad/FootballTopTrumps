b0b55007b0b5d8cd33032abc0af506375acff8ee
Gavin-Persad <gavinapersad@gmail.com>
2024-06-19 14:38:39 +0100

    shuffled cards now being split to two arrays playerCards and computerCards

--- src/app/game/game.js
+++ src/app/game/game.js
@@ -1,18 +1,32 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import footballPlayerData from '../playerData';
-import react, { useState, useEffect } from 'react';
 
 export default function Game() {
+	const [isShuffled, setIsShuffled] = useState(false);
+
 	useEffect(() => {
-		shuffleAndSelectCards(footballPlayerData);
-	}, []);
+		if (!isShuffled) {
+			shuffleAndSelectCards(footballPlayerData);
+			setIsShuffled(true);
+		}
+	}, [isShuffled]);
 
 	function shuffleAndSelectCards(footballPlayerData) {
 		let playerCards = [];
 		let computerCards = [];
 
-		const shuffledData = footballPlayerData[1].sort(() => Math.random() - 0.5);
-		console.log(shuffledData);
+		const shuffledData = [...footballPlayerData[1]].sort(
+			() => Math.random() - 0.5
+		);
+
+		for (let i = 0; i < shuffledData.length; i++) {
+			if (i % 2 === 0) {
+				playerCards.push(shuffledData[i]);
+			} else {
+				computerCards.push(shuffledData[i]);
+			}
+		}
 	}
 }
